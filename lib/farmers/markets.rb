e42d8ebf6cf4d1316af727a77a062740b8086968
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-20 15:09:23 +0000

    first commit

---
+++ lib/farmers/markets.rb
@@ -0,0 +1,8 @@
+require "farmers/markets/version"
+
+module Farmers
+  module Markets
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/farmers/markets/version.rb
@@ -0,0 +1,5 @@
+module Farmers
+  module Markets
+    VERSION = "0.1.0"
+  end
+end
