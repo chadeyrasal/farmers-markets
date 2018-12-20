b3052d5feae877900ff6afcdf406f2e356c93f9f
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-20 16:35:05 +0000

    Small changes in the structure of the lib directory

--- lib/farmers/markets.rb
+++
@@ -1,8 +0,0 @@
-require "farmers/markets/version"
-
-module Farmers
-  module Markets
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
---
+++ lib/farmers_markets/markets.rb
@@ -0,0 +1,5 @@
+require "farmers_markets/version"
+
+module FarmersMarkets
+  class Error < StandardError; end
+end
